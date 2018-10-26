793164a9d9739642263d0024b57ed95bbdbee790
anshuman000 <anshu6633@gmail.com>
2018-10-26 13:04:55 +0530

    make changes

--- upload-apk.sh
+++ upload-apk.sh
@@ -19,6 +19,8 @@ cd apk
 \cp -r ../app/build/outputs/apk/debug/output.json debug-output.json
 \cp -r ../app/build/outputs/apk/release/output.json release-output.json
 
+\cp -r ../app/build/outputs/apk/release/output.json release-output.json
+
 # Signing Apps
 
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
